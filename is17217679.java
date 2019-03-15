91fc8604db499a625151720e73d91292083edc66
Damian <damianskrzypek351@interia.eu>
2019-03-15 15:53:56 +0000

    Cleaned up the code

--- is17217679.java
+++ is17217679.java
@@ -9,41 +9,46 @@ import javax.swing.JOptionPane;
  */
 
 public class is17217679
-{
+{   
+    private static int[][] finalState;
+
     public static void main(String[] args)
     {
         String startState = "";
         String endState = "";
+        int[][] initialState;
 
         startState = JOptionPane.showInputDialog("Enter the start state: ");
         while(!validateInput(startState))
-        {
             startState = JOptionPane.showInputDialog("Wrong format, renter the start state: ");
-        }
 
         endState = JOptionPane.showInputDialog("Enter the end state: ");
         while(!validateInput(endState))
-        {
             endState = JOptionPane.showInputDialog("Wrong format, renter the end state: ");
-        }
+
+        initialState = convertInput(startState);
+        finalState = convertInput(endState);
+
+        possibleMovements(initialState);
+        possibleMovements(finalState);
     }
 
-    public static int[] convertInput(String in) 
+    public static int[][] convertInput(String in) 
     {
-        String [] temp = new int[9];
+        String [] temp;
         temp = (in.split(" "));
         int count = 0;
 
         int [][] board = new int [3][3];
 
         for(int i = 0; i < 3; i++)
-            for(int j = 0; j < 3; j++){
+            for(int j = 0; j < 3; j++)
+            {
                 board[i][j] = Integer.parseInt(temp [count]);
                 count++;
             }
-                
-            
 
+        return board;
     }
 
     public static boolean validateInput(String in)
@@ -55,20 +60,16 @@ public class is17217679
         if(temp.length == 9)
         {
             for(int i = 0; i < temp.length;i++)
-            {
                 numbers.add(i);
-            }
+            
             for(int i = 0; i < temp.length;i++)
-                 if(numbers.contains(Integer.parseInt(temp[i])) && (!checkIndividual.contains(temp[i])))
-                 {
+                if(numbers.contains(Integer.parseInt(temp[i])) && (!checkIndividual.contains(temp[i])))
                     checkIndividual.add(temp[i]);
-                 }
             
             if(checkIndividual.size() == numbers.size())
-            {
                 return true;
-            }
         }
+
         return false;
     }
 
@@ -80,41 +81,35 @@ public class is17217679
         int i = 0, j = 0;
 
         for(i = 0; i < state.length && !stop; i++)
-        {
             for(j = 0; j < state[i].length && !stop; j++)
-            {
                 stop = (state[i][j] == 0);
-            }
-        }
+        
         i--;
         j--;
 
         if(i - 1 > -1)
-            System.out.println(state[i-1][j] + " to the south");
+            System.out.println("-> " + state[i-1][j] + " to the south");
         if(i + 1 < 3)
-            System.out.println(state[i+1][j] + " to the north");
+            System.out.println("-> " + state[i+1][j] + " to the north");
         if(j - 1 > -1)
-            System.out.println(state[i][j - 1] + " to the east");
+            System.out.println("-> " + state[i][j - 1] + " to the east");
         if(j + 1 < 3)
-            System.out.println(state[i][j + 1] + " to the west");
+            System.out.println("-> " + state[i][j + 1] + " to the west");
+
+        System.out.println("Value of h: " + calculateH(state) + "\n");
     }
 
     // Numbers out of place or how far each number is from its final state
-    public static int calculateH(int[][] state, int[][] finalState)
+    public static int calculateH(int[][] state)
     {
         int counter = 0;
 
         for(int i = 0; i < state.length; i++)
-        {
             for(int j = 0; j < state[i].length; j++)
-            {
                 if(state[i][j] != finalState[i][j])
-                {
                     counter++;
-                }
-            }
-        }
-    return counter;
+        
+        return counter;
     }
 
     // This might not need to be here, g is which 
@@ -129,9 +124,7 @@ public class is17217679
         for(int i = 0; i < state.length; i++)
         {
             for(int j = 0; j < state[i].length; j++)
-            {
                 System.out.print(state[i][j] + " ");
-            }
             System.out.println();
         }
     }
